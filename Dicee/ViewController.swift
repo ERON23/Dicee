72956071bcf9d62c907061310d2b66f95125b8fc
maceo <eriick23@yahoo.com>
2018-02-25 21:44:50 -0600

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by eortiz on 1/16/18.
+//  Copyright © 2018 eortxz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+    
+    let diceArray = ["dice1","dice2","dice3","dice4","dice5","dice6"]
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImages()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
+    func updateDiceImages() {
+        // casting is needed because arc4random only takes Uint 32 numbers
+        // therefore casting int into Uint 32
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+        
+    }
+    
+    // when you finish shaking your iphone it detects that you shaked the divice
+    // then it changes dices.
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        
+        updateDiceImages()
+        
+    }
+    
+}
+
